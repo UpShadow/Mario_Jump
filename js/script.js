c1784f87f7ef10b1ea2f3642d2067f2a4b387348
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-09-12 01:11:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const text = document.querySelector('.game-over-text');
 
 const jump = () => {
     mario.classList.add('jump');
     
     setTimeout(() => {
         mario.classList.remove('jump');
-     }, 500);
+    }, 500);
 }
 
 const loop = setInterval(() => {
-
     const pipePosition = pipe.offsetLeft;
-    const marioPostion = window.getComputedStyle(mario).bottom;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); // + convert a string to number
+    
+    text.style.display = 'none';
 
-    if(pipePosition <= 120) {
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        text.style.display = 'block';
+
+        clearInterval(loop);
     }
 
 }, 10);
